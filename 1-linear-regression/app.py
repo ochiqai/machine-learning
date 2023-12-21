7a5033e1415fd8645dd1c14e3315c8ba43421b75
Samariddin Kushmuratov <bekmezonaliu@gmail.com>
2023-12-21 21:54:01 +0500

    added

---
+++ 1-linear-regression/app.py
@@ -0,0 +1,27 @@
+import math
+import pickle
+import numpy as np
+import gradio as gr
+
+
+def visible_component():
+    return gr.update(visible=True)
+def calculate(room):
+    yangi_data_point = np.array(int(room)).reshape(-1, 1)
+    yangi_narx = model.predict(yangi_data_point)
+    return int(math.ceil(yangi_narx[0]))
+
+
+# load model
+with open('models/linerReg-Samariddin.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with gr.Blocks() as demo:
+    room = gr.Textbox(label="Count of room")
+    calculate_btn = gr.Button("Calculate")
+    price = gr.Textbox(label="Price", visible=False)
+    calculate_btn.click(fn=calculate, inputs=room, outputs=price, api_name="calculate").then(fn=visible_component, outputs=price)
+
+
+if __name__ == "__main__":
+    demo.launch()
---
+++ 1-linear-regression/train_save_model.py
@@ -0,0 +1,21 @@
+import pickle
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+
+## data
+xonalar = [1, 2, 3, 5, 6, 7]
+narxlar = [150, 200, 250, 350, 400, 450]
+
+# listni numpyga o'tkazishimiz kerak, chunki sklearn numpy asosida ishlaydi
+X = np.array(xonalar).reshape(-1, 1)
+y = np.array(narxlar)
+print(X.shape)
+print(y.shape)
+
+#train qilish jarayoni
+model = LinearRegression()
+model.fit(X, y)
+with open('models/linerReg-Samariddin.pkl','wb') as f:
+    pickle.dump(model,f)
